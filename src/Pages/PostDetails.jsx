f464c76105302e46070f1767a4f24e742f295661
Soumya Banerjee <soumyabanerjee783@gmail.com>
2022-07-07 23:07:19 +0530

    Comments ...

--- src/Pages/PostDetails.jsx
+++ src/Pages/PostDetails.jsx
@@ -14,6 +14,7 @@ import { toast } from 'react-toastify'
 import { updatePost } from '../services/posts'
 import { logUserOut } from '../slices/authSlice'
 import { checkUserToken } from '../services/checkUserToken'
+import PostDetailsComment from '../components/Posts/Post/PostDetailsComment'
 
 TimeAgo.addLocale(en)
 const timeAgo = new TimeAgo('en-US')
@@ -68,6 +69,33 @@ function PostDetails() {
     dispatch(updatePost(updatedPost, post._id))
   }
 
+  const handleCommentDel = (delComment) => {
+    if (!user) {
+      toast.error('You are not Logged In!')
+      return
+    }
+
+    // checks if login token is still valid
+    if (!checkUserToken()) {
+      toast.info('Session Expired!')
+      dispatch(logUserOut())
+      return
+    }
+    let post = postData
+    let updatedPost = {}
+
+    if (post.comments.map((comment) => comment._id).includes(delComment._id)) {
+      updatedPost = {
+        ...post,
+        comments: post.comments.filter(
+          (comment) => comment._id !== delComment._id
+        ),
+      }
+      setPostData(updatedPost)
+      dispatch(updatePost(updatedPost, post._id))
+    }
+  }
+
   useEffect(() => {
     getPost()
   }, [])
@@ -83,75 +111,77 @@ function PostDetails() {
       postData
 
     return (
-      <div className='transparentCard max-w-[1100px] mx-auto p-6 flex flex-col-reverse gap-y-6 md:gap-y-0 md:gap-x-6 md:flex-row md:justify-between'>
-        {/* text */}
-        <div className='w-full max-w-[500px] md:max-w-[50%] lg:max-w-[calc(100%-500px)] mx-auto md:mx-0 md:flex md:flex-col md:justify-between'>
-          <div>
+      <div className='flex flex-col md:flex-row max-w-[1000px] mx-auto gap-x-6 items-start justify-between md:mb-6'>
+        <div className='transparentCard w-full max-w-[500px] md:max-w-[unset] mx-auto md:mx-0 md:w-[50%] p-6 flex flex-col-reverse gap-y-6 md:gap-y-0'>
+          {/* text */}
+          <div className='w-full max-w-[500px] mx-auto'>
+            <div>
+              <p className='text-3xl font-semibold mt-4'>{title}</p>
+              {tags && (
+                <div className='flex items-center gap-x-2 mt-1'>
+                  {tags.map((tg, index) => (
+                    <p key={index} className='text-gray-500 text-lg'>
+                      #{tg}
+                    </p>
+                  ))}
+                </div>
+              )}
+              <p className='max-w-[98%] text-justify leading-7 break-words w-full my-4'>
+                {message}
+              </p>
+            </div>
+            <div className='flex justify-between'>
+              <div
+                className='flex items-center space-x-1 cursor-pointer'
+                onClick={() => handleOnLike(likes)}
+              >
+                {user && likes.length > 0 && likes.includes(user.result._id) ? (
+                  <AiFillLike className='text-blue-600 text-xl' />
+                ) : (
+                  <AiOutlineLike className='text-blue-600 text-xl' />
+                )}
+                <p className='text-xl'>{likes.length}</p>
+              </div>
+              <div>
+                <p className='text-gray-700 text-right text-xl flex items-center gap-x-2'>
+                  <span className='font-bold text-black'>Created By:</span>
+                  {name}
+                </p>
+                <p className='text-gray-700 text-right text-xl flex items-center gap-x-2'>
+                  <span className='font-bold text-black'>Posted :</span>
+                  {timeAgo.format(new Date(createdAt))}
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* image */}
+          <div className='w-full max-w-[500px] mx-auto'>
             <p
               onClick={() => navigate(-1)}
-              className='hidden md:flex items-center cursor-pointer text-gray-500 hover:text-gray-800'
+              className='mb-4 flex items-center cursor-pointer text-gray-500 hover:text-gray-800'
             >
               <IoIosArrowBack /> Go Back
             </p>
-            <p className='text-3xl font-semibold md:mt-4'>{title}</p>
-            {tags && (
-              <div className='flex items-center gap-x-2 mt-1'>
-                {tags.map((tg, index) => (
-                  <p key={index} className='text-gray-500 text-lg'>
-                    #{tg}
-                  </p>
-                ))}
-              </div>
-            )}
-            <p className='max-w-[98%] text-justify leading-7 break-words w-full my-4'>
-              {message}
-            </p>
-          </div>
-          <div className='flex justify-between'>
-            <div
-              className='flex items-center space-x-1 cursor-pointer'
-              onClick={() => handleOnLike(likes)}
-            >
-              {user && likes.length > 0 && likes.includes(user.result._id) ? (
-                <AiFillLike className='text-blue-600 text-xl' />
-              ) : (
-                <AiOutlineLike className='text-blue-600 text-xl' />
-              )}
-              <p className=' text-xl'>{likes.length}</p>
-            </div>
-            <div>
-              <p className='text-gray-700 text-right text-xl flex items-center gap-x-2'>
-                <span className='font-bold text-black'>Created By:</span>
-                {name}
-              </p>
-              <p className='text-gray-700 text-right text-xl flex items-center gap-x-2'>
-                <span className='font-bold text-black'>Posted :</span>
-                {timeAgo.format(new Date(createdAt))}
-              </p>
+            <div className='w-full h-max flex justify-center items-center'>
+              <LazyLoadImage
+                alt='post'
+                effect='blur'
+                src={
+                  selectedFile !== ''
+                    ? selectedFile
+                    : '../Assets/placeholder.webp'
+                }
+                className='w-full rounded-lg object-contain shadow-xl'
+              />
             </div>
           </div>
         </div>
-        {/* image */}
-        <div className='w-full max-w-[500px] md:max-w-[50%] lg:max-w-[500px] mx-auto md:mx-0'>
-          <p
-            onClick={() => navigate(-1)}
-            className='flex items-center cursor-pointer text-gray-500 hover:text-gray-800 md:hidden'
-          >
-            <IoIosArrowBack /> Go Back
-          </p>
-          <div className='w-full h-max md:h-[400px] lg:h-[500px] flex justify-center items-center'>
-            <LazyLoadImage
-              alt='post'
-              effect='blur'
-              src={
-                selectedFile !== ''
-                  ? selectedFile
-                  : '../Assets/placeholder.webp'
-              }
-              className='w-full md:max-w-[400px] md:max-h-[400px] lg:max-h-[500px] lg:max-w-[500px] rounded-lg object-contain shadow-xl'
-            />
-          </div>
-        </div>
+        <PostDetailsComment
+          user={user}
+          id={postId}
+          post={postData}
+          handleCommentDel={handleCommentDel}
+        />
       </div>
     )
   }
