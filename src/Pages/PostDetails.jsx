b33cbac2d71a865aa7a2375fa8e6715a4ef8d4db
Soumya Banerjee <soumyabanerjee783@gmail.com>
2022-07-07 14:57:40 +0530

    posts details improvement + like

--- src/Pages/PostDetails.jsx
+++ src/Pages/PostDetails.jsx
@@ -8,6 +8,12 @@ import { LazyLoadImage } from 'react-lazy-load-image-component'
 import 'react-lazy-load-image-component/src/effects/blur.css'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
+import { IoIosArrowBack } from 'react-icons/io'
+import { AiOutlineLike, AiFillLike } from 'react-icons/ai'
+import { toast } from 'react-toastify'
+import { updatePost } from '../services/posts'
+import { logUserOut } from '../slices/authSlice'
+import { checkUserToken } from '../services/checkUserToken'
 
 TimeAgo.addLocale(en)
 const timeAgo = new TimeAgo('en-US')
@@ -16,9 +22,10 @@ function PostDetails() {
   const location = useLocation()
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const { loading } = useSelector((state) => state.loading)
-  const [postData, setPostData] = useState(null)
   const postId = location.pathname.split('/')[2]
+  const { loading } = useSelector((state) => state.loading)
+  const { user } = useSelector((state) => state.auth)
+  const [postData, setPostData] = useState(null)
   console.log('post Id', postId)
 
   const getPost = async () => {
@@ -34,6 +41,33 @@ function PostDetails() {
     }
   }
 
+  const handleOnLike = (likes) => {
+    if (!user) {
+      toast.error('You are not Logged In!')
+      return
+    }
+
+    // checks if login token is still valid
+    if (!checkUserToken()) {
+      toast.info('Session Expired!')
+      dispatch(logUserOut())
+      return
+    }
+    let post = postData
+    let updatedPost = {}
+    const existingLike = likes.filter((like) => like === user.result._id)
+    if (existingLike.length === 0) {
+      updatedPost = { ...post, likes: [...likes, user.result._id] }
+    } else {
+      updatedPost = {
+        ...post,
+        likes: likes.filter((like) => like !== user.result._id),
+      }
+    }
+    setPostData(updatedPost)
+    dispatch(updatePost(updatedPost, post._id))
+  }
+
   useEffect(() => {
     getPost()
   }, [])
@@ -45,44 +79,78 @@ function PostDetails() {
       </div>
     )
   } else {
-    const { title, message, name, tags, selectedFile, createdAt } = postData
+    const { title, message, name, tags, selectedFile, createdAt, likes } =
+      postData
 
     return (
-      <div className='max-w-max mx-auto flex flex-col-reverse items-center md:flex-row md:items-start justify-between md:space-x-6 transparentCard p-6 mb-6 relative my-8'>
+      <div className='transparentCard max-w-[1100px] mx-auto p-6 flex flex-col-reverse gap-y-6 md:gap-y-0 md:gap-x-6 md:flex-row md:justify-between'>
         {/* text */}
-        <p onClick={() => navigate(-1)} className='absolute -top-6 left-0'>
-          Go Back
-        </p>
-        <div className='w-full mt-6 md:mt-0 md:w-[50%]'>
-          <p className='text-2xl font-semibold'>{title}</p>
-          {tags && (
-            <div className='flex items-center gap-x-2'>
-              {tags.map((tg) => (
-                <p className='text-gray-500'>#{tg}</p>
-              ))}
+        <div className='w-full max-w-[500px] md:max-w-[50%] lg:max-w-[calc(100%-500px)] mx-auto md:mx-0 md:flex md:flex-col md:justify-between'>
+          <div>
+            <p
+              onClick={() => navigate(-1)}
+              className='hidden md:flex items-center cursor-pointer text-gray-500 hover:text-gray-800'
+            >
+              <IoIosArrowBack /> Go Back
+            </p>
+            <p className='text-3xl font-semibold md:mt-4'>{title}</p>
+            {tags && (
+              <div className='flex items-center gap-x-2 mt-1'>
+                {tags.map((tg, index) => (
+                  <p key={index} className='text-gray-500 text-lg'>
+                    #{tg}
+                  </p>
+                ))}
+              </div>
+            )}
+            <p className='max-w-[98%] text-justify leading-7 break-words w-full my-4'>
+              {message}
+            </p>
+          </div>
+          <div className='flex justify-between'>
+            <div
+              className='flex items-center space-x-1 cursor-pointer'
+              onClick={() => handleOnLike(likes)}
+            >
+              {user && likes.length > 0 && likes.includes(user.result._id) ? (
+                <AiFillLike className='text-blue-600 text-xl' />
+              ) : (
+                <AiOutlineLike className='text-blue-600 text-xl' />
+              )}
+              <p className=' text-xl'>{likes.length}</p>
             </div>
-          )}
-          <p className='leading-7 my-4 break-words w-full min-h-[100px] max-w-[600px]'>
-            {message}
-          </p>
-          <p className='text-gray-700 flex items-center gap-x-2'>
-            <span className='font-bold text-black'>Created By:</span>
-            {name}
-          </p>
-          <p className='text-gray-700 flex items-center gap-x-2'>
-            <span className='font-bold text-black'>Posted :</span>
-            {timeAgo.format(new Date(createdAt))}
-          </p>
+            <div>
+              <p className='text-gray-700 text-right text-xl flex items-center gap-x-2'>
+                <span className='font-bold text-black'>Created By:</span>
+                {name}
+              </p>
+              <p className='text-gray-700 text-right text-xl flex items-center gap-x-2'>
+                <span className='font-bold text-black'>Posted :</span>
+                {timeAgo.format(new Date(createdAt))}
+              </p>
+            </div>
+          </div>
         </div>
         {/* image */}
-        <div className='w-full md:w-[50%] flex justify-center md:justify-end postDetailsImg'>
-          <LazyLoadImage
-            alt='post'
-            effect='blur'
-            src={
-              selectedFile !== '' ? selectedFile : '../Assets/placeholder.webp'
-            }
-          />
+        <div className='w-full max-w-[500px] md:max-w-[50%] lg:max-w-[500px] mx-auto md:mx-0'>
+          <p
+            onClick={() => navigate(-1)}
+            className='flex items-center cursor-pointer text-gray-500 hover:text-gray-800 md:hidden'
+          >
+            <IoIosArrowBack /> Go Back
+          </p>
+          <div className='w-full h-max md:h-[400px] lg:h-[500px] flex justify-center items-center'>
+            <LazyLoadImage
+              alt='post'
+              effect='blur'
+              src={
+                selectedFile !== ''
+                  ? selectedFile
+                  : '../Assets/placeholder.webp'
+              }
+              className='w-full md:max-w-[400px] md:max-h-[400px] lg:max-h-[500px] lg:max-w-[500px] rounded-lg object-contain shadow-xl'
+            />
+          </div>
         </div>
       </div>
     )
